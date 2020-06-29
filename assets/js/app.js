937ac8faeecc8704ed0473f2f07947b339d81bd8
toran billups <toranb@gmail.com>
2020-06-29 06:31:38 -0500

    opt in for event metadata (changelog for 13X)

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,43 @@ import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  metadata: {
+    click: (e, el) => {
+      return {
+        altKey: e.altKey,
+        shiftKey: e.shiftKey,
+        ctrlKey: e.ctrlKey,
+        metaKey: e.metaKey,
+        x: e.x || e.clientX,
+        y: e.y || e.clientY,
+        pageX: e.pageX,
+        pageY: e.pageY,
+        screenX: e.screenX,
+        screenY: e.screenY,
+        offsetX: e.offsetX,
+        offsetY: e.offsetY,
+        detail: e.detail || 1,
+      }
+    },
+    keydown: (e, el) => {
+      return {
+        altGraphKey: e.altGraphKey,
+        altKey: e.altKey,
+        code: e.code,
+        ctrlKey: e.ctrlKey,
+        key: e.key,
+        keyIdentifier: e.keyIdentifier,
+        keyLocation: e.keyLocation,
+        location: e.location,
+        metaKey: e.metaKey,
+        repeat: e.repeat,
+        shiftKey: e.shiftKey
+      }
+    }
+  }
+})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
