c88f7e32d2d41d18b0b2676241fd0006075e868f
toran billups <toranb@gmail.com>
2021-11-28 16:33:11 -0600

    tweak the url with check origin

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,4 +7,7 @@ config :todo, TodoWeb.Endpoint,
     port: 4000,
     transport_options: [socket_opts: [:inet6]]
   ],
+  check_origin: [
+    "https://game1101.gigalixirapp.com"
+  ],
   secret_key_base: "PfmabSaMe6hM5cTQw68voWDwvGd0DqYkkH3k56wp9+qgzXP3qZhqcTSrWQJMS7w1"
